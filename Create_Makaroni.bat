be8a27aa917f0d5901e239f5d4ab98d76a0a6629
SosMakaroni <balint0016@hotmail.com>
2022-02-11 16:08:16 +0100

    New generation

--- Create_Makaroni.bat
+++ Create_Makaroni.bat
@@ -4,6 +4,4 @@ copy /b "C:\Users\balin\Documents\GitHub\HunTramSet\Vehicles_code\*.nml" "C:\Use
 nmlc -c --grf Villamosok.grf Villamosok.nml
 rd /s /q ".nmlcache\"
 copy "Villamosok.grf" "C:\Users\balin\Documents\OpenTTD\content_download\newgrf"
-del "C:\Users\balin\Documents\GitHub\HunTramSet\Villamosok.nml.bak"
-del "C:\Users\balin\Documents\GitHub\HunTramSet\Create_Makaroni.bat.bak"
 pause>nul
